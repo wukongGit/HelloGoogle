2e26d76b347fb855bbd9e31adfada9259ff610c9
suncheng <sunchenggx@163.com>
2017-05-16 17:33:35 +0800

    remove application

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world! Hello Google!')
+        self.response.write('Hello world! Hello Google! i am sunc glade to meet you!')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
